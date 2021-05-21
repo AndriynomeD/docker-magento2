f5399ecb12d0a3ecce33daaa23c68244c523c0e5
AndriynomeD <andriynomed.work@gmail.com>
2021-05-21 20:18:21 +0300

    refactored config structure & combine builder

--- builder.php
+++ builder.php
@@ -7,13 +7,23 @@
  *
  * This extends from the original Builder in the `docker-magento` repository.
  */
-class Builder2
+class ConfigBuilder
 {
-    const DEFAULT_CONFIG_KEY = "php-containers";
-    const DEFAULT_CONFIG_FILE_NAME = "config.json";
-    const DEFAULT_CONFIG_FILE = __DIR__ . DIRECTORY_SEPARATOR . self::DEFAULT_CONFIG_FILE_NAME;
-    const DEFAULT_TEMPLATE_DIR = __DIR__ . DIRECTORY_SEPARATOR . "src/";
-    const DEFAULT_DESTINATION_DIR = __DIR__ . DIRECTORY_SEPARATOR . "containers" . DIRECTORY_SEPARATOR . "php";
+    const CONFIG_FILE_NAME = 'config.json';
+    const CONFIG_FILE = __DIR__ . DIRECTORY_SEPARATOR . self::CONFIG_FILE_NAME;
+    const TEMPLATE_DIR = __DIR__ . DIRECTORY_SEPARATOR . 'src';
+    const GENERAL_CONFIG_KEY = 'general-config';
+    
+    /* Php containers consts */
+    const PHP_CONTAINERS_CONFIG_KEY = 'php-containers';
+    const PHP_CONTAINERS_TEMPLATE_DIR = 'phpContainers';
+    const PHP_CONTAINERS_DESTINATION_DIR = __DIR__ . DIRECTORY_SEPARATOR . 'containers' . DIRECTORY_SEPARATOR . 'php';
+    const DEFAULT_COMPOSERVERSION = '1.10.17';
+    
+    /* docker-compose consts */
+    const DOCKER_COMPOSE_TEMPLATE_FILE = 'docker-compose.tml';
+    const DOCKER_COMPOSE_DESTINATION_FILE = __DIR__ . DIRECTORY_SEPARATOR . 'docker-compose.yml';
+    
     const DEFAULT_EXECUTABLE_PERMISSIONS = 0755;
     const DEFAULT_VERBOSE_LEVEL = 1;
     
@@ -22,88 +32,167 @@ class Builder2
      *
      * @var array
      */
-    protected $build_config = [];
-    
-    /**
-     * Directory to load template files from.
-     *
-     * @var string
-     */
-    protected $template_dir;
-    
-    /**
-     * Destination directory for generated files.
-     *
-     * @var string
-     */
-    protected $destination_dir;
-    
+    protected $config = [];
+
     /**
      * File permissions for executable files.
      *
      * @var int
      */
-    protected $executable_permissions;
+    protected $executablePermissions;
     
     /**
      * Verbosity level.
      *
      * @var int
      */
-    protected $verbose_level;
+    protected $verboseLevel;
 
     public function __construct($options = [])
     {
-        $this->template_dir = $options["template_dir"] ?? static::DEFAULT_TEMPLATE_DIR;
-        $this->destination_dir = $options["destination_dir"] ?? static::DEFAULT_DESTINATION_DIR;
-        $this->executable_permissions = $options["executable_file_permissions"] ?? static::DEFAULT_EXECUTABLE_PERMISSIONS;
-        $this->verbose_level = $options["verbose"] ?? static::DEFAULT_VERBOSE_LEVEL;
+        $this->executablePermissions = $options['executable_file_permissions'] ?? static::DEFAULT_EXECUTABLE_PERMISSIONS;
+        $this->verboseLevel = $options['verbose'] ?? static::DEFAULT_VERBOSE_LEVEL;
         
-        $this->loadConfig($options["config_file"] ?? static::DEFAULT_CONFIG_FILE);
+        $this->loadConfig(static::CONFIG_FILE);
     }
-    
+
     /**
      * Build the files described in the loaded config.
      */
     public function run()
     {
-        foreach ($this->build_config as $name => $config) {
+        $this->buildPhpContainers();
+        $this->buildDockerCompose();
+    }
+    
+    /**
+     * Build the files described in the loaded config.
+     */
+    private function buildPhpContainers()
+    {
+        $templateDirPath = self::TEMPLATE_DIR . DIRECTORY_SEPARATOR
+            . self::PHP_CONTAINERS_TEMPLATE_DIR . DIRECTORY_SEPARATOR;
+        $generalConfig = $this->config[self::GENERAL_CONFIG_KEY];
+        $buildPhpVersion = $generalConfig['PHP_VERSION'];
+        $defaultPhpContainerConfig = [
+            'composerVersion' => self::DEFAULT_COMPOSERVERSION,
+            'databaseType' => $generalConfig['DOCKER_DB']['TYPE'],
+            'databaseVersion' => $generalConfig['DOCKER_DB']['VERSION'],
+            'specificPackages' => []
+        ];
+        $defaultSpecificPackages = [
+            'gd' => true,
+            'imagick' => false,
+            'calendar' => false,
+            'ioncube' => false,
+            'libsodiumfix' => false
+        ];
+        $defaultFileVariables = [
+            '_disable_variables' => false,
+            'executable' => false
+        ];
+        $phpContainersConfig = $this->config[self::PHP_CONTAINERS_CONFIG_KEY];
+        foreach ($phpContainersConfig as $name => $phpContainerConfig) {
+            if ($phpContainerConfig['version'] != $buildPhpVersion) {
+                // delete not used or just ignore them?
+                continue;
+            }
             $this->verbose(sprintf("Building '%s'...", $name), 1);
-            foreach ($config["files"] as $file_name => $variables) {
-                $destination_file = $this->getDestinationFile($file_name, $config);
-                $contents = "";
-                
-                if ($template_file = $this->getTemplateFile($file_name, $config)) {
-                    // Merge global variables to the template variables
-                    $variables["version"] = $config["version"];
-                    $variables["flavour"] = $config["flavour"];
-                    $variables["imageSpecificPhpExtensions"] = $config["phpExtensions"];
-                    $variables["flavourSpecificPackages"] = array_key_exists("flavourPackages", $config) ? $config["flavourPackages"] : [];
-                    $variables["xdebugVersion"] = $config["xdebugVersion"];
-                    $variables["composerVersion"] = array_key_exists("composerVersion", $config) ? $config["composerVersion"]: '1.10.17';
+            $configFiles = $phpContainerConfig['files'];
+            unset($phpContainerConfig['files']);
+            $phpContainerConfig['phpVersion'] = $phpContainerConfig['version'];
+            $templateConfig = [
+                'templateDirPath' => $templateDirPath,
+                'version' => $phpContainerConfig['version'],
+                'flavour' => $phpContainerConfig['flavour'],
+            ];
+            foreach ($configFiles as $filename => $variables) {
+                $contents = '';
+                if ($templateFile = $this->getTemplateFile($filename, $templateConfig)) {
+                    /* list of all variables
+                    $variables = [
+                        'composerVersion', 'databaseType', 'databaseVersion',
+                        'phpVersion', 'flavour', 'packages', 'phpExtensions', 'specificPackages', 'xdebugVersion',
+                        '_disable_variables', 'executable',
+                    ];
+                    */
+                    $variables = array_merge(
+                        $defaultPhpContainerConfig,
+                        $phpContainerConfig,
+                        $defaultFileVariables,
+                        $variables
+                    );
+                    $variables['specificPackages'] = array_merge($defaultSpecificPackages, $variables['specificPackages']);
 
                     // Determine whether we should load with the template renderer, or whether we should straight up
                     // just load the file from disk.
-                    if ($variables['_disable_variables'] ?? false) {
-                        $contents = file_get_contents($template_file);
+                    if ($variables['_disable_variables']) {
+                        $contents = file_get_contents($templateFile);
                     } else {
-                        $contents = $this->renderTemplate($template_file, $variables);
+                        $contents = $this->renderTemplate($templateFile, $variables);
                     }
-                    
+
                     $contents = str_replace('{{generated_by_builder}}', 'This file is automatically generated. Do not edit directly.', $contents);
                 }
+
+                $destinationFile = $this->getDestinationFile(
+                    $filename,
+                    $phpContainerConfig,
+                    self::PHP_CONTAINERS_DESTINATION_DIR
+                );
+                $this->verbose(sprintf("\tWriting '%s'...", $destinationFile), 2);
+                $this->writeFile($destinationFile, $contents);
                 
-                $this->verbose(sprintf("\tWriting '%s'...", $destination_file), 2);
-                $this->writeFile($destination_file, $contents);
-                
-                if ($variables["executable"] ?? false) {
-                    $this->verbose(sprintf("\tUpdating permissions on '%s' to '%o'...", $destination_file, $this->executable_permissions), 2);
-                    $this->setFilePermissions($destination_file, $this->executable_permissions);
+                if ($variables['executable'] ) {
+                    $this->verbose(sprintf("\tUpdating permissions on '%s' to '%o'...", $destinationFile, $this->executablePermissions), 2);
+                    $this->setFilePermissions($destinationFile, $this->executablePermissions);
                 }
             }
         }
     }
-    
+
+    private function buildDockerCompose()
+    {
+        $templateDirPath = self::TEMPLATE_DIR . DIRECTORY_SEPARATOR;
+        $generalConfig = $this->config[self::GENERAL_CONFIG_KEY];
+        $defaultDockerComposeConfig = [
+            'PHP_VERSION' => '7.4',
+            'M2_VERSION' => '2.4.*',
+            'M2_INSTALL_DEMO' => [],
+            'DOCKER_DB' => [],
+            'DOCKER_ADDITIONAL_SERVICES' => []
+        ];
+        $defaultAdditionalServicesConfig = [
+            'external_elasticsearch'=> true,
+            'internal_elasticsearch'=> false,
+            'varnish'=> true,
+            'cron'=> true,
+            'redis'=> false,
+            'rabbitmq'=> false
+        ];
+        $this->verbose(sprintf("Building '%s'...", 'docker-compose.yml'), 1);
+        $templateConfig = [
+            'templateDirPath' => $templateDirPath,
+            'version' => 'none',
+            'flavour' => 'none',
+        ];
+        $filename = self::DOCKER_COMPOSE_TEMPLATE_FILE;
+        $contents = '';
+        if ($templateFile = $this->getTemplateFile($filename, $templateConfig)) {
+            $variables = array_merge($defaultDockerComposeConfig, $generalConfig);
+            $variables['DOCKER_ADDITIONAL_SERVICES'] = array_merge($defaultAdditionalServicesConfig, $variables['DOCKER_ADDITIONAL_SERVICES']);
+
+            $contents = $this->renderTemplate($templateFile, $variables);
+            $contents = str_replace('{{generated_by_builder}}', 'This file is automatically generated. Do not edit directly.', $contents);
+        }
+
+        $destinationFile = self::DOCKER_COMPOSE_DESTINATION_FILE;
+        $this->verbose(sprintf("\tWriting '%s'...", $destinationFile), 2);
+        $this->writeFile($destinationFile, $contents);
+        $this->setFilePermissions($destinationFile, $this->executablePermissions);
+    }
+
+
     /**
      * Load the build configuration from the given file.
      *
@@ -115,25 +204,31 @@ class Builder2
     protected function loadConfig($file)
     {
         if (!(file_exists($file) && is_readable($file))) {
-            throw new Exception(sprintf("File %s not exist or not readable!", self::DEFAULT_CONFIG_FILE_NAME));
+            throw new Exception(sprintf("File %s not exist or not readable!", self::CONFIG_FILE_NAME));
         }
 
         $config = json_decode(file_get_contents($file), true);
 
         if (!is_array($config)
-            || (!array_key_exists(static::DEFAULT_CONFIG_KEY, $config)
-                || !is_array($config[static::DEFAULT_CONFIG_KEY]))
+            || !array_key_exists(static::GENERAL_CONFIG_KEY, $config)
+            || (!array_key_exists(static::PHP_CONTAINERS_CONFIG_KEY, $config)
+                || !is_array($config[static::PHP_CONTAINERS_CONFIG_KEY]))
         ) {
             throw new Exception(sprintf("Invalid configuration in %s!", $file));
         }
 
-        $this->build_config = $config[static::DEFAULT_CONFIG_KEY];
+        $this->config = $config;
         
         return $this;
     }
     
     /**
      * Return the template file name for the given file.
+     * Example:
+     * Dockerfile-7.4-cli
+     * Dockerfile-7.4
+     * Dockerfile-cli - found
+     * Dockerfile
      *
      * @param string $filename
      * @param array  $config
@@ -142,16 +237,15 @@ class Builder2
      */
     protected function getTemplateFile($filename, $config)
     {
-        $potential_file_names = [
-            sprintf("%s-%s-%s", $filename, $config["version"], $config["flavour"]),
-            sprintf("%s-%s", $filename, $config["version"]),
-            sprintf("%s-%s", $filename, $config["flavour"]),
+        $templateDirPath = $config['templateDirPath'];
+        $potentialFilenames = [
+            sprintf("%s-%s-%s", $filename, $config['version'], $config['flavour']),
+            sprintf("%s-%s", $filename, $config['version']),
+            sprintf("%s-%s", $filename, $config['flavour']),
             $filename,
         ];
-        
-        foreach ($potential_file_names as $potential_file_name) {
-            $path = $this->template_dir . DIRECTORY_SEPARATOR . $potential_file_name;
-
+        foreach ($potentialFilenames as $potentialFilename) {
+            $path =  $templateDirPath . $potentialFilename;
             if (file_exists($path) && is_readable($path)) {
                 return $path;
             }
@@ -163,34 +257,34 @@ class Builder2
     /**
      * Get the destination for the given file.
      *
-     * @param string $file_name
+     * @param string $filename
      * @param array  $config
      *
      * @return string
      */
-    protected function getDestinationFile($file_name, $config)
+    protected function getDestinationFile($filename, $config, $destinationFolder)
     {
         return implode(DIRECTORY_SEPARATOR, [
-            $this->destination_dir,
-            $config["version"] . '-' . $config["flavour"],
-            $file_name,
+            $destinationFolder,
+            $config['version'] . '-' . $config['flavour'],
+            $filename,
         ]);
     }
     
     /**
      * Render the given template file using the provided variables and return the resulting output.
      *
-     * @param string $template_file
+     * @param string $templateFile
      * @param array  $variables
      *
      * @return string
      */
-    protected function renderTemplate($template_file, $variables)
+    protected function renderTemplate($templateFile, $variables)
     {
         extract($variables, EXTR_OVERWRITE);
         ob_start();
         
-        include $template_file;
+        include $templateFile;
         
         $output = ob_get_clean();
         
@@ -200,15 +294,15 @@ class Builder2
     /**
      * Write the contents to the given file.
      *
-     * @param string $file_name
+     * @param string $filename
      * @param string $contents
      *
      * @return $this
      * @throws Exception
      */
-    protected function writeFile($file_name, $contents)
+    protected function writeFile($filename, $contents)
     {
-        $directory = dirname($file_name);
+        $directory = dirname($filename);
         
         // If the directory doesn't created then try to create the directory.
         if (!is_dir($directory)) {
@@ -218,8 +312,8 @@ class Builder2
             }
         }
         
-        if (file_put_contents($file_name, $contents) === false) {
-            throw new Exception(sprintf("Failed to write %s!", $file_name));
+        if (file_put_contents($filename, $contents) === false) {
+            throw new Exception(sprintf("Failed to write %s!", $filename));
         }
         
         return $this;
@@ -228,14 +322,14 @@ class Builder2
     /**
      * Update the permissions on the given file.
      *
-     * @param string $file_name
+     * @param string $filename
      * @param int    $permissions
      *
      * @return $this
      */
-    protected function setFilePermissions($file_name, $permissions = 0644)
+    protected function setFilePermissions($filename, $permissions = 0644)
     {
-        chmod($file_name, $permissions);
+        chmod($filename, $permissions);
         
         return $this;
     }
@@ -251,7 +345,7 @@ class Builder2
      */
     protected function verbose($message, $level = 1, $newline = true)
     {
-        if ($level <= $this->verbose_level) {
+        if ($level <= $this->verboseLevel) {
             printf("%s%s", $message, $newline ? PHP_EOL : "");
         }
         
@@ -284,5 +378,5 @@ if (isset($args["q"])) {
     $options["verbose"] = count($args["v"]);
 }
 
-$builder = new Builder2($options);
+$builder = new ConfigBuilder($options);
 $builder->run();
